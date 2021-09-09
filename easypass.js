deeb5cdb53f655a50315e7e79c128daf56da8545
pongsathon346 <pongsathon_j@cmu.ac.th>
2021-09-09 23:16:55 +0700

    build others function

--- easypass.js
+++ easypass.js
@@ -14,13 +14,44 @@ var cardObj = [
     {cardId: 3, cardName: "silva", cardMoney: 1000}
 ]
 
+var array = []
+
+pay = (id,idLoca) => {
+    const inde = cardObj.findIndex((item) => {
+        return item.cardId === id;
+    })
+    console.log(inde);
+    const [{ cardMoney }] = cardObj.filter((item,key) => {
+        if (inde === key){
+            return item
+        }
+    })   
+
+    const index = location.findIndex((item) => {
+        
+        return item.localId === idLoca;
+    });
+
+    const [{ toll }] = location.filter((item, key) => {
+        if (index === key) {
+            return item
+        }
+    });
+    
+    let result = cardMoney-toll;
+    cardObj[inde].cardMoney = result;
+    array.push(new Payment(inde+1,cardObj[inde].cardName,index+1,location[index].localName,toll,result))
+
+    return {message:`${cardObj[inde].cardName} pay ${toll} at station ${location[index].localName} balance ${result}`}
+    // console.table(cardObj);
+    // console.log(cardObj[inde].cardName + ' pay '+ location[index].toll + ' baht at '+  location[index].localName+' station ');
+}
+
 fillmoney = (id,amount) => {
-    console.log('id '+id);
     const index = cardObj.findIndex((item) => {
         console.log('item.cardId '+item.cardId)
         return item.cardId == id
     })
-    console.log('index '+index);
     const [{cardMoney}] = cardObj.filter((item,key) => {
         if(index ==key){
             return item
@@ -35,12 +66,12 @@ fillmoney = (id,amount) => {
 } 
 
 checkmoney = (id) => {
-    console.log('id '+id);
     const index = cardObj.findIndex((item) => {
         console.log('item.cardId '+item.cardId)
         return item.cardId == id
     })
     console.table(cardObj[index]);
+    
     return cardObj[index]
 }
 
@@ -48,8 +79,30 @@ showlocation = () => {
     console.table(location);
     return location
 }
+
+const Payment = function(id,name,locaid,locaname,toll,money) {
+    this.id=id;
+    this.name=name;
+    this.locaid=locaid;
+    this.locaname=locaname;
+    this.payed=toll;
+    this.balance=money;
+}
+
+Payment.prototype.getPayment = function() {
+    return  this.id,this.name
+}
+
+showpayment = () => {
+    return array
+}
+
+
+
 module.exports={
     fillmoney: fillmoney,
     showlocation: showlocation,
     checkmoney: checkmoney,
+    pay: pay,
+    showpayment:showpayment,
 }
--- server.js
+++ server.js
@@ -1,31 +1,78 @@
 var http = require('http');
 var url = require('url');
 var array = []
-const {fillmoney, showlocation} = require('./easypass');
-
+const {fillmoney, showlocation,pay,checkmoney,showpayment} = require('./easypass');
+ 
 http.createServer(function (req, res) {
     var request_path = url.parse(req.url, true);
     var message = '';
+    var status = 200;
+    var data = "";
+    
+    let response_object = {
+        statusCode: status,
+        message: message,
+        data: data
+
+    }
 
     switch (request_path.pathname) {
         case '/fillmoney':
-            try {
-                let showIem =  fillmoney(request_path.query.id,request_path.query.amount);
-                message += 'success';
-                data = showIem
-                status = 200;
-            } catch (e) {
-                message += e;
-                status = 400;
-                console.log(e);
-            }
+            if(req.method == "POST") {
+                let req_input = [];
+                req
+                   .on("data", (chunk) => {
+                       req_input.push(chunk);
+                   }) 
+                   .on("end", () => {
+                       let json_data = JSON.parse(Buffer.concat(req_input).toString());
+                       try {
+                           response_object.data = fillmoney(json_data.id,json_data.amount);
+                           response_object.message = "success";
+                           res.writeHead(200, {'Content-Type': 'application/json' });
+                           res.end(JSON.stringify(response_object));
+                       } catch (err) {
+                           message += err;
+                           console.log(err);
+                       }
+                   });
+            } else {
+                throw "method not match";
+            }   
             break;
+
+        case '/pay':
+            if(req.method == "POST") {
+                let req_input = [];
+                req
+                    .on("data", (chunk) => {
+                        req_input.push(chunk);
+                    }) 
+                    .on("end", () => {
+                        let json_data = JSON.parse(Buffer.concat(req_input).toString());
+                        try {
+                            response_object.data = pay(json_data.id,json_data.idLoca);
+                            response_object.message = "success";
+                            res.writeHead(200, {'Content-Type': 'application/json' });
+                            res.end(JSON.stringify(response_object));
+                        } catch (err) {
+                            message += err;
+                            console.log(err);
+                        }
+                    });
+            } else {
+                throw "method not match";
+            }   
+            break;
+
         case '/showlocation':
             try {
                 let showIem =  showlocation();
                 message += 'success';
-                data = showIem
+                response_object.data = showIem
                 status = 200;
+                res.writeHead(200, {'Content-Type': 'application/json' });
+                res.end(JSON.stringify(response_object));
             } catch (e) {
                 message += e;
                 status = 400;
@@ -36,8 +83,24 @@ http.createServer(function (req, res) {
             try {
                 let showIem =  checkmoney(request_path.query.id);
                 message += 'success';
-                data = showIem
+                response_object.data = showIem
                 status = 200;
+                res.writeHead(200, {'Content-Type': 'application/json' });
+                res.end(JSON.stringify(response_object));
+            } catch (e) {
+                message += e;
+                status = 400;
+                console.log(e);
+            }
+            break;   
+        case '/showpayment':
+            try {
+                let showIem =  showpayment(request_path.query.id);
+                message += 'success';
+                response_object.data = showIem
+                status = 200;
+                res.writeHead(200, {'Content-Type': 'application/json' });
+                res.end(JSON.stringify(response_object));
             } catch (e) {
                 message += e;
                 status = 400;
@@ -52,18 +115,7 @@ http.createServer(function (req, res) {
             break;
     }
     
-    let response_object = {
-        status: status,
-        message: message,
-        data: data
-
-    }
-    
-    
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify(response_object));
+   
 
 }).listen(6060);
 console.log('Server started at port 6060');
